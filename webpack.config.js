e71b4d5d2f862b66cd561ad236be99db945d0e42
Devam M <devam493@gmail.com>
2019-08-20 13:57:43 -0400

    react & webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    '@babel/polyfill',
+    './src/index.js'
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+         use: ['style-loader', 'css-loader']
+      } 
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    filename: 'bundle.js'
+  }
+};
