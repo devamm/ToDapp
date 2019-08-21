09cde80e4c7d1ef69d515b50f809247340876d46
Devam M <devam493@gmail.com>
2019-08-21 08:35:31 -0400

    web3 setup and created ABI

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import Web3 from 'web3';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            account: ""
+        }
+
+        this.connectToBlockChain = this.connectToBlockChain.bind(this);
+    }
+
+    componentDidMount(){
+       this.connectToBlockChain();
+    }
+
+    async connectToBlockChain(){
+        //YEET
+        const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+        const account = await web3.eth.getAccounts();
+        this.setState({account: account[0]});
+    }
+    
     render(){
+       // console.log(this.state.account)
         return (
             <div>
                 <h1>Hello!</h1>
+                <h3>Your Account: {this.state.account} </h3>
             </div>
         )
     }
